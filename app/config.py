ef18a4d511d86177d47ebe16627e0a48aa7d1a6e
Layka <95246690+GolotyukAndrey@users.noreply.github.com>
2026-03-29 11:28:11 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
 ENV_PATH = PROJECT_ROOT / ".env"
 
-# Явно грузим .env
 load_dotenv(dotenv_path=ENV_PATH)
 
 class Settings:
